c7afbcd687347d61750cd01b63b9205fc04a02b6
Your Name <you@example.com>
2023-10-07 17:39:19 -0400

    base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    // server configurations
+    hmr: {
+      protocol: 'ws',  // using 'ws' for WebSockets
+      timeout: 5000,  // a timeout for HMR
+    },
+  },
 })
